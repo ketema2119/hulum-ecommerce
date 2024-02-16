74a6d6893aca63524e3a38c332c6e184e4f3bd95
ketema2119 <ketemabekele18@gmail.com>
2024-02-16 18:41:58 +0100

    Updates

---
+++ precache-manifest.b8125758af33ededdcba0b26dac9febb.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "c27dc89165ddd122b95e",
+    "url": "/hulum-ecommerce/static/css/main.edb2c7be.chunk.css"
+  },
+  {
+    "revision": "c27dc89165ddd122b95e",
+    "url": "/hulum-ecommerce/static/js/main.71bd0815.chunk.js"
+  },
+  {
+    "revision": "6f07fb05b661c2677239",
+    "url": "/hulum-ecommerce/static/js/runtime~main.5b984250.js"
+  },
+  {
+    "revision": "bd796259387b0abbf982",
+    "url": "/hulum-ecommerce/static/js/2.750131ff.chunk.js"
+  },
+  {
+    "revision": "34bd52478836c347c940f3ab9804f3c9",
+    "url": "/hulum-ecommerce/static/media/SearchIcon.34bd5247.svg"
+  },
+  {
+    "revision": "aa4a2b4c1cb00a40aeefbec79e08373d",
+    "url": "/hulum-ecommerce/static/media/CartHero1.aa4a2b4c.svg"
+  },
+  {
+    "revision": "86e2c6b4bdd0590d51555e401d3c6510",
+    "url": "/hulum-ecommerce/static/media/NewsLetterBg.86e2c6b4.png"
+  },
+  {
+    "revision": "9d807c52ba8c87414c409239c76d4af8",
+    "url": "/hulum-ecommerce/static/media/CartCard.9d807c52.svg"
+  },
+  {
+    "revision": "af3ffc3572ca3333abc16c2c7cc3f132",
+    "url": "/hulum-ecommerce/static/media/StarFull.af3ffc35.svg"
+  },
+  {
+    "revision": "3fd153bc04f5707cdde054f57023b54f",
+    "url": "/hulum-ecommerce/static/media/StarHalf.3fd153bc.svg"
+  },
+  {
+    "revision": "0e5dcf2dfd81676854c35a0f57b32b41",
+    "url": "/hulum-ecommerce/static/media/Grid.0e5dcf2d.svg"
+  },
+  {
+    "revision": "177219c26fe732489257b0c502f30524",
+    "url": "/hulum-ecommerce/static/media/Hamburger.177219c2.svg"
+  },
+  {
+    "revision": "53ba69bda82162e37d978a83736277e3",
+    "url": "/hulum-ecommerce/static/media/grayStar.53ba69bd.svg"
+  },
+  {
+    "revision": "ae30e4c080b982553879c55c782fbacd",
+    "url": "/hulum-ecommerce/static/media/goldStar.ae30e4c0.svg"
+  },
+  {
+    "revision": "b079c10bcc5e28021c8b0e7fb0fc4d06",
+    "url": "/hulum-ecommerce/static/media/close-circle-outlined.b079c10b.svg"
+  },
+  {
+    "revision": "22c45ea6ceb7911e6f488c38102a2547",
+    "url": "/hulum-ecommerce/static/media/close-for-cart-itmes.22c45ea6.svg"
+  },
+  {
+    "revision": "173f1bd19ec9f80cf92d7e9b24374331",
+    "url": "/hulum-ecommerce/static/media/productAddedTick.173f1bd1.svg"
+  },
+  {
+    "revision": "54c2e8c3c182769287ce90956ec79fe6",
+    "url": "/hulum-ecommerce/static/media/FAcebook.54c2e8c3.svg"
+  },
+  {
+    "revision": "7afb4ceccbf53a42a5081c060e9afa77",
+    "url": "/hulum-ecommerce/static/media/Youtube.7afb4cec.svg"
+  },
+  {
+    "revision": "bb70b6cec9fe6b1eaa4ef6d00c63ab5c",
+    "url": "/hulum-ecommerce/static/media/Telegram.bb70b6ce.svg"
+  },
+  {
+    "revision": "89b27c6de9771e6917ed1700a990e64f",
+    "url": "/hulum-ecommerce/static/media/LinkedIn.89b27c6d.svg"
+  },
+  {
+    "revision": "45146e9ca89f411b790147f004fada98",
+    "url": "/hulum-ecommerce/static/media/Twitter.45146e9c.svg"
+  },
+  {
+    "revision": "1565f6c633efdf08ad48b6ed48c3b6f1",
+    "url": "/hulum-ecommerce/static/media/Whatsapp.1565f6c6.svg"
+  },
+  {
+    "revision": "8612c4f92da3182d65c8b7d72788de17",
+    "url": "/hulum-ecommerce/static/media/Logo.8612c4f9.png"
+  },
+  {
+    "revision": "7a40fe674a8fb13aa71b666b46bf6131",
+    "url": "/hulum-ecommerce/static/media/Account.7a40fe67.svg"
+  },
+  {
+    "revision": "fc33d64a05de102db42ff0c3f3a6f5ed",
+    "url": "/hulum-ecommerce/static/media/Heart.fc33d64a.svg"
+  },
+  {
+    "revision": "33e095a7ffff911594296840827ec7d3",
+    "url": "/hulum-ecommerce/static/media/Cart.33e095a7.svg"
+  },
+  {
+    "revision": "768dbd80eba8687db6b2ce288f21fdb7",
+    "url": "/hulum-ecommerce/static/media/Construction.768dbd80.svg"
+  },
+  {
+    "revision": "e2873a160be9d8c8d5b0a6b79bad6b74",
+    "url": "/hulum-ecommerce/static/media/Electronics.e2873a16.svg"
+  },
+  {
+    "revision": "e203ec53ed957e6656d4dcc88c9b4290",
+    "url": "/hulum-ecommerce/static/media/Fashin & beatuy.e203ec53.svg"
+  },
+  {
+    "revision": "9f85d9852828db8f25a7222b8ca99ad3",
+    "url": "/hulum-ecommerce/static/media/Food.9f85d985.svg"
+  },
+  {
+    "revision": "1b04109c4128dd996e76f1f1a47ffb2a",
+    "url": "/hulum-ecommerce/static/media/Home & Garden.1b04109c.svg"
+  },
+  {
+    "revision": "5e71bfeadcd4954fc55fd15e2a83f8a9",
+    "url": "/hulum-ecommerce/static/media/Kids.5e71bfea.svg"
+  },
+  {
+    "revision": "3fe2bace6e9e4a1826afe5d8b019bbf5",
+    "url": "/hulum-ecommerce/static/media/Service.3fe2bace.svg"
+  },
+  {
+    "revision": "4a4abf1d6b0781298895a8cf38e80b14",
+    "url": "/hulum-ecommerce/static/media/Vehicle.4a4abf1d.svg"
+  },
+  {
+    "revision": "a2894cfecaf2c2ef82702cd72376a4f5",
+    "url": "/hulum-ecommerce/static/media/shoe.a2894cfe.jpg"
+  },
+  {
+    "revision": "005e023df0e6f29a6cd64274eaa7ccf8",
+    "url": "/hulum-ecommerce/static/media/HeroPic.005e023d.png"
+  },
+  {
+    "revision": "0a78c21214c6d62edefaaec2fbb44d4b",
+    "url": "/hulum-ecommerce/index.html"
+  }
+];
